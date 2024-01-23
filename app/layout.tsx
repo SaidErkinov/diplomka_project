c3d36c250fc33197ed0a00b8b25cb750f5a6fae5
saiderkinov <saidkhan.erkinov@gmail.com>
2024-01-23 16:54:14 +0500

    members modal added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
@@ -25,6 +26,7 @@ export default function RootLayout({
           'bg-white dark:bg-[#313338]'
           )}>
           <ThemeProvider attribute="class" defaultTheme="dark" enableSystem = {false} storageKey="diplomka-theme">
+            <ModalProvider />
             {children}
           </ThemeProvider>
           </body>
