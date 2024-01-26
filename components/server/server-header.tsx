0c2039c8daf0fa74adc8b1f97689b2e6e8a6010a
saiderkinov <saidkhan.erkinov@gmail.com>
2024-01-26 14:35:47 +0500

    leave/delete modals added

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -70,6 +70,7 @@ export const ServerHeader = ({
         )}
         {isModerator && (
           <DropdownMenuItem
+          onClick = {() => onOpen("createChannel")}
           className="px-3 py-2 text-sm cursor-pointer"
           >
             Create Channel
@@ -81,6 +82,7 @@ export const ServerHeader = ({
         )}
         {isAdmin && (
           <DropdownMenuItem
+          onClick={() => onOpen("deleteServer", { server })}
           className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
           >
             Delete Server
@@ -89,6 +91,7 @@ export const ServerHeader = ({
         )}
         {!isAdmin && (
           <DropdownMenuItem
+          onClick={() => onOpen("leaveServer", {server})}
           className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
           >
             Leave Server
