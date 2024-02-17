8063f9f22797318d91a35b883bac731781a1cc92
saiderkinov <saidkhan.erkinov@gmail.com>
2024-02-17 16:00:34 +0500

    socketio, chat components and bug fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,9 @@ import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
+import { SocketProvider } from "@/components/providers/socket-provider";
 import { cn } from "@/lib/utils";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -26,8 +28,12 @@ export default function RootLayout({
           'bg-white dark:bg-[#313338]'
           )}>
           <ThemeProvider attribute="class" defaultTheme="dark" enableSystem = {false} storageKey="diplomka-theme">
-            <ModalProvider />
-            {children}
+            <SocketProvider>
+              <ModalProvider />
+              <QueryProvider>
+                {children}
+              </QueryProvider>
+            </SocketProvider>
           </ThemeProvider>
           </body>
       </html>
